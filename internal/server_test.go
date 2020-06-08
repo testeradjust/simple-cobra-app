ae48936180a6a2af13e5ed4137b0967c03c3f1dc
serj <srdjan@adjust.com>
2020-06-08 09:44:38 +0200

    server increment version func

--- internal/server_test.go
+++ internal/server_test.go
@@ -12,3 +12,11 @@ func TestNewServer(t *testing.T) {
 	assert.Equal(t, 1, server.Version)
 	assert.Equal(t, "s1", server.Name)
 }
+
+func TestServer_IncrementVersion(t *testing.T) {
+	server := NewServer(1, "s1")
+	require.NotNil(t, server)
+	assert.Equal(t, 1, server.Version)
+	server.IncrementVersion()
+	assert.Equal(t, 2, server.Version)
+}
