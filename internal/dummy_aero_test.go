b3dfaddfc58adecaf47c5f3708036eb92f38fbed
serj <srdjan@adjust.com>
2020-06-09 16:26:00 +0200

    aerospike tests hostname and port

--- internal/dummy_aero_test.go
+++ internal/dummy_aero_test.go
@@ -7,14 +7,19 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const (
+	aerospikeHostname = "localhost"
+	aerospikePort     = 3000
+)
+
 func TestNewDummyAero(t *testing.T) {
-	dummyAero, err := NewDummyAero("172.28.128.3", 3000)
+	dummyAero, err := NewDummyAero(aerospikeHostname, aerospikePort)
 	assert.NoError(t, err)
 	assert.NotNil(t, dummyAero, "aero client returned should not be nil")
 }
 
 func TestDummyAero_TestConnection(t *testing.T) {
-	dummyAero, err := NewDummyAero("172.28.128.3", 3000)
+	dummyAero, err := NewDummyAero(aerospikeHostname, aerospikePort)
 	assert.NoError(t, err)
 	require.NotNil(t, dummyAero, "aero client returned should not be nil")
 
