1139d7769e4b487646effe170de1726e0f633a6d
serj <srdjan@adjust.com>
2020-06-09 17:38:30 +0200

    aerospike setup script - travis

---
+++ travis/aerospike-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o pipefail # abort on errors in pipeline
+set -e          # abort on errors
+
+# download, for ubuntu 18.04 bionic
+wget -O aerospike.tgz 'https://www.aerospike.com/download/server/latest/artifact/ubuntu18'
+# untar
+tar xvzf aerospike.tgz # will extract: aerospike-server-community-5.0.0.4-ubuntu18.04
+# install
+cd $(ls | grep "aerospike-server-community")
+sudo ./asinstall
+# go back to source
+cd ..
